175240a2c742a3d75206b3ec2494394313d96dec
JuanJakobo <github@johannsn.com>
2022-09-08 14:24:38 +0200

    Resort menu and fix same number for 2 buttons

--- src/handler/mainMenu.cpp
+++ src/handler/mainMenu.cpp
@@ -60,13 +60,13 @@ int MainMenu::createMenu(bool filePicker, bool loggedIn, iv_menuhandler handler)
             {ITEM_HEADER, 0, _menu, NULL},
             //show logged in
             {loggedIn ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 101, _syncFolder, NULL},
-            {loggedIn ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 102, _logout, NULL},
             {loggedIn ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 103, _sortBy, NULL},
+            {loggedIn ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 102, _logout, NULL},
             //show if filePicker is shown
-            {filePicker ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 103, _chooseFolder, NULL},
+            {filePicker ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 104, _chooseFolder, NULL},
             //show always
-            {ITEM_ACTIVE, 104, _info, NULL},
-            {ITEM_ACTIVE, 105, _exit, NULL},
+            {ITEM_ACTIVE, 105, _info, NULL},
+            {ITEM_ACTIVE, 106, _exit, NULL},
             {0, 0, NULL, NULL}};
 
     OpenMenu(mainMenu, 0, _panelMenuBeginX, _panelMenuBeginY, handler);
